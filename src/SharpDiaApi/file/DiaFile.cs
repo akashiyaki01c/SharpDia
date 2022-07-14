07a44a06e524354d0be8dbce4dd45e666f2daeb7
akashiyaki <datenshi.gab@hotmail.com@hotmail.com>
2022-07-15 04:12:31 +0900

    dotnet new

---
+++ src/SharpDiaApi/file/DiaFile.cs
@@ -0,0 +1,8 @@
+using System;
+
+/// <summary>
+/// 一つのダイヤグラムファイルを表すクラス
+/// </summary>
+public class DiaFile {
+
+}
---
+++ src/SharpDiaApp/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SharpDiaApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
